8615970a901c866c83d008591a3f2a463d149473
SneakyDragon <fabio.f.dasilva@hotmail.com>
2021-07-16 14:31:14 +0100

    css for Login Page added

--- client/src/features/loginPage/login.js
+++ client/src/features/loginPage/login.js
@@ -5,6 +5,7 @@ import {selectCurrentUser, login} from "../currentUser/currentUserSlice";
 import {Signup} from '../signup/signup';
 import { useContext } from 'react';
 import { myContext } from '../../Context';
+import './login.css';
 
 export const LoginPage = () =>{
     const [name, setName] = React.useState("");
@@ -47,22 +48,24 @@ export const LoginPage = () =>{
         window.open("http://localhost:5000/auth/google", "_self");
     }
     return(
-        <div>
-            <form onSubmit={tryLogin}>
+        <div id = "loginFormContainer">
+            <form onSubmit={tryLogin} id="loginForm">
                 <p>Username:</p>
                 <input type="text" name="username" value={name} onChange={handleUsernameChange}></input>
                 <p>Password:</p>
                 <input type="password" name="password" value={password} onChange={handlePasswordChange}></input><br />
-                <button type="submit">Login</button>
+                <button type="submit" id="loginButton">Login</button>
             </form>
-                <p>Not registered? <NavLink exact to="/signup">Signup here</NavLink></p>
+                <p id="registerLink">Not registered? <NavLink exact to="/signup">Signup here</NavLink></p>
                 <Switch>
                     <Route path ='/store'>
                         <Signup />
                     </Route>
                 </Switch>
 
-            <button onClick={googleLogin}>GoogleLogin</button>
+            <p id="registerLink"> Or log in with Google</p><button onClick={googleLogin} id="googleLogin">Google Login</button>
+            <p id="registerLink">(Might need to contact me via email first so I can add you to the list of accepted emails for google login, blame google)</p>
+            <p id="registerLink">(fabio.f.dasilva@hotmail.com)</p>
         </div>
 
     )
