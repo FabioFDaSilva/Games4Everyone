9b0c58e7468cbf702474526718045dc73c578007
SneakyDragon <fabio.f.dasilva@hotmail.com>
2021-07-19 18:19:58 +0100

    adding SSL to heroku

--- db.js
+++ db.js
@@ -2,15 +2,18 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const devConfig = {
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    database:process.env.DATABASE,
-    port:process.env.PORT
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    database:process.env.PG_DATABASE,
+    port:process.env.PG_PORT
 }
 
 const proConfig = {
-    connectionString : process.env.DATABASE_URL
+    connectionString : process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
 }
 const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
